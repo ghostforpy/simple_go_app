c7605bb34994d5291f00307420c2b419f762831c
ghostforpy <ghostformobile@gmail.com>
2022-01-13 20:34:14 +0300

    add db connection

--- internals/db/db.go
+++ internals/db/db.go
@@ -1,20 +1,26 @@
 package db
 
 import (
+	"database/sql"
+	"fmt"
 	"os"
-	"strings"
 
-	"github.com/go-pg/pg/v10"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/driver/pgdriver"
 )
 
-func NewPostgresDB() *pg.DB {
-	conn := pg.Connect(&pg.Options{
-		Addr: strings.Join(
-			[]string{os.Getenv("POSTGRES_SERVER"), os.Getenv("POSTGRES_PORT")}, ":",
-		),
-		User:     os.Getenv("POSTGRES_USER"),
-		Password: os.Getenv("POSTGRES_PASSWORD"),
-		Database: os.Getenv("POSTGRES_DB"),
-	})
+func NewPostgresDB() *bun.DB {
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_SERVER"),
+		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_DB"))
+
+	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
+
+	conn := bun.NewDB(sqldb, pgdialect.New())
 	return conn
 }
