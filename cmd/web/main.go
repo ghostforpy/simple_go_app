396fad93581eeebc83869810636000158a77299b
ghostforpy <ghostformobile@gmail.com>
2022-01-13 20:33:53 +0300

    add profiler db query

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,27 +2,33 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/ghostforpy/simple_go_app/internals/db"
 	"github.com/ghostforpy/simple_go_app/internals/handlers"
 	"github.com/gorilla/mux"
+	"github.com/uptrace/bun/extra/bundebug"
 )
 
 func main() {
-	r := mux.NewRouter()
-	rootRouter := handlers.NewRootHandler()
-	rootRouter.RegisterRoutes(r)
+
 	conn := db.NewPostgresDB()
+	conn.AddQueryHook(bundebug.NewQueryHook(
+		bundebug.WithVerbose(true),
+		bundebug.FromEnv("BUNDEBUG"),
+	))
 	defer conn.Close()
 	ctx := context.Background()
-
-	if err := conn.Ping(ctx); err != nil {
+	if _, err := conn.ExecContext(ctx, "SELECT 1"); err != nil {
 		panic(err)
+	} else {
+		log.Println("Connected to DB!")
 	}
-	fmt.Print("all right")
+	r := mux.NewRouter()
+	rootRouter := handlers.NewRootHandler()
+	rootRouter.RegisterRoutes(conn, ctx, r)
+
 	log.Println("Запуск сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", r)
 	log.Fatal(err)
